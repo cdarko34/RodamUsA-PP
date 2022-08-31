a9878bff42658d9b1a3b3f645ccb9658642a19c6
cdarko <carolinasnd2@hotmail.com>
2022-08-30 23:22:49 -0500

    subida RodamUs js

---
+++ js/index.js
@@ -0,0 +1,11 @@
+var membresia = 200;
+
+function descuento(membresia , params) {
+    
+}
+
+const codigo = prompt("Ingresa tu código de descuento");
+
+if(codigo = "DESCUENTO50"){
+    console.log("Cuentas con $50 de descuento tu total es" + membresia -50);
+}
